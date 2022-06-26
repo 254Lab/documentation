5f16adf34ce74ef62468aa02c33cbb0cc1fe4faa
Will <wotieno1@email.cpcc.edu>
2022-06-26 15:32:01 -0400

    next.config.js and package.json

--- next.config.js
+++ next.config.js
@@ -1,12 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  experimental: {
+    outputStandalone: true,
+  },
   images: {
     loader: "akamai",
     path: "",
   },
   basePath: "/documentation",
-  assetPrefix: "/documentation/",
+  assetPrefix: "/documentation",
 };
 
 module.exports = nextConfig;
