cb3d1e1f366a4d75c3ab4be005e345d87a1c359a
Will <wotieno1@email.cpcc.edu>
2022-06-26 12:49:14 -0400

    gh-pages configuration

--- next.config.js
+++ next.config.js
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  images: {
+    loader: "akamai",
+    path: "",
+  },
+  env: {
+    PUBLIC_URL: "https://your-organization-or-username.github.io/my-nextjs-app",
+    assetPrefix: "./",
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
