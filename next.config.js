afb2d3a910183f1fe6bd377c235bcd13e42a3d52
Will <wotieno1@email.cpcc.edu>
2022-06-26 14:49:12 -0400

    another next.config.js configuration testing

--- next.config.js
+++ next.config.js
@@ -1,4 +1,6 @@
 /** @type {import('next').NextConfig} */
+const withPlugins = require("next-compose-plugins");
+const optimizedImages = require("next-optimized-images");
 const nextConfig = {
   reactStrictMode: true,
   images: {
@@ -9,6 +11,8 @@ const nextConfig = {
     PUBLIC_URL: "https://254Lab.github.io/documentation",
     assetPrefix: "./",
   },
+  basePath: "/documentation",
+  assetPrefix: "/documentation/",
 };
 
 module.exports = nextConfig;
