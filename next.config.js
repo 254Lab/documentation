76f8d5621b130f59828e2c6c1112f56cbb06657e
Will <wotieno1@email.cpcc.edu>
2022-06-26 14:54:07 -0400

    another next.config.js configuration testing

--- next.config.js
+++ next.config.js
@@ -1,6 +1,4 @@
 /** @type {import('next').NextConfig} */
-const withPlugins = require("next-compose-plugins");
-const optimizedImages = require("next-optimized-images");
 const nextConfig = {
   reactStrictMode: true,
   images: {
